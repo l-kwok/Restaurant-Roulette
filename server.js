6f211eb3f1c538d30d68c556678ef94c6a1c818c
Linden Kwok <lindenstkwok@gmail.com>
2021-04-29 18:51:23 -0700

    server updates for production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const axios = require("axios");
-const path = require('path');
+const path = require("path");
 //temp to reduce api calls to google servers
 const fs = require("fs");
 const dummyData = require("./placesData.json");
@@ -13,49 +13,89 @@ const app = express();
 const port = process.env.PORT || 5000;
 // const googleMapsClient = new Client({}); //instantiate google maps client
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/api/places", (req, res) => {
-	const request = req.body;
-	if (request.foodType === "") {
-		request.foodType =
-			"japanese,korean,vietnamese,hotdogs,chinese,newamerican,tradeamerican,indpak";
-	}
-	axios
-		.get(
-			`https://api.yelp.com/v3/businesses/search?categories=${request.foodType}&latitude=${request.location.lat}&longitude=${request.location.lng}&radius=${request.range}&open_now=${request.openNow}&limit=50`,
-			{
-				headers: {
-					Authorization: `Bearer ${process.env.API_KEY_YELP}`,
-				},
-			}
-		)
-		.then((response) => {
-			// console.log(response.data);
-			res.send(response.data);
-			// fs.writeFile("placesData.json", JSON.stringify(response.data), (err) => {
-			// 	if (err) {
-			// 		throw err;
-			// 	}
-			// 	console.log("JSON data is saved.");
-			// });
-		})
-		.catch((e) => {
-			console.log(`Businesses API Error : ${e}`);
-			if (e.status === 429) {
-				res.sendStatus(429); //Quota Reached
-			} else {
-				res.sendStatus(500); //Internal Server Error
-			}
-		});
-});
-
 if (process.env.NODE_ENV === "production") {
-	const buildDir = '/client/build'
+	const buildDir = "/client/build";
 	app.use(express.static(path.join(__dirname, buildDir)));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+
 	app.get("/*", (req, res) => {
 		res.sendFile(path.join(__dirname, buildDir, "index.html"));
+	});
+
+	app.post("/api/places", (req, res) => {
+		const request = req.body;
+		if (request.foodType === "") {
+			request.foodType =
+				"japanese,korean,vietnamese,hotdogs,chinese,newamerican,tradeamerican,indpak";
+		}
+		axios
+			.get(
+				`https://api.yelp.com/v3/businesses/search?categories=${request.foodType}&latitude=${request.location.lat}&longitude=${request.location.lng}&radius=${request.range}&open_now=${request.openNow}&limit=50`,
+				{
+					headers: {
+						Authorization: `Bearer ${process.env.API_KEY_YELP}`,
+					},
+				}
+			)
+			.then((response) => {
+				// console.log(response.data);
+				res.send(response.data);
+				// fs.writeFile("placesData.json", JSON.stringify(response.data), (err) => {
+				// 	if (err) {
+				// 		throw err;
+				// 	}
+				// 	console.log("JSON data is saved.");
+				// });
+			})
+			.catch((e) => {
+				console.log(`Businesses API Error : ${e}`);
+				if (e.status === 429) {
+					res.sendStatus(429); //Quota Reached
+				} else {
+					res.sendStatus(500); //Internal Server Error
+				}
+			});
+	});
+}
+
+if (process.env.NODE_ENV === "development") {
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+
+	app.post("/api/places", (req, res) => {
+		const request = req.body;
+		if (request.foodType === "") {
+			request.foodType =
+				"japanese,korean,vietnamese,hotdogs,chinese,newamerican,tradeamerican,indpak";
+		}
+		axios
+			.get(
+				`https://api.yelp.com/v3/businesses/search?categories=${request.foodType}&latitude=${request.location.lat}&longitude=${request.location.lng}&radius=${request.range}&open_now=${request.openNow}&limit=50`,
+				{
+					headers: {
+						Authorization: `Bearer ${process.env.API_KEY_YELP}`,
+					},
+				}
+			)
+			.then((response) => {
+				// console.log(response.data);
+				res.send(response.data);
+				// fs.writeFile("placesData.json", JSON.stringify(response.data), (err) => {
+				// 	if (err) {
+				// 		throw err;
+				// 	}
+				// 	console.log("JSON data is saved.");
+				// });
+			})
+			.catch((e) => {
+				console.log(`Businesses API Error : ${e}`);
+				if (e.status === 429) {
+					res.sendStatus(429); //Quota Reached
+				} else {
+					res.sendStatus(500); //Internal Server Error
+				}
+			});
 	});
 }
 
